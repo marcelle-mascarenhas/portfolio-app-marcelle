514ee79ba61f814c28e2c49ebbd0d7fdf3e951d9
marcelle-mascarenhas <marcellebts@gmail.com>
2022-12-02 12:06:08 -0300

    feat: update baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://api-node-portfolio.herokuapp.com/",
+    baseURL: "https://back-portfolio-marcelle.glitch.me/",
 });
 
 export default instance;
